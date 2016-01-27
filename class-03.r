eeaea3a5c86b6660cd6a258812cc4dee217af84f
Miklos Kalman <Miklos Kalman>
2016-01-27 18:57:48 +0100

    update

--- class-03.r
+++ class-03.r
@@ -3,11 +3,12 @@ library(ggplot2)
 ggplot(mtcars, aes(x = hp)) + geom_histogram(binwidth = 50)
 
 #grouped bar char
-ggplot(mtcars, aes(x = carb)) + geom_bar() *
+#ggplot(mtcars, aes(x = carb)) + geom_bar() +facet_grid(~am)
 
+mtcars$am <- factor(mtcars$am)
+ggplot(mtcars, aes(x = carb,fill=am)) + geom_bar(position = 'dodge',bin)
 
+#boxplot for hp and carb
+ggplot(mtcars, aes(x = factor(carb), y=hp)) + geom_boxplot()
 
-%ggplot(mtcars, aes(x = carb, y = hp)) + geom_boxplot()
-
-ggplot(mtcars, aes(x = hp, y=wt, fill=carb)) + geom_area()
 
