861aeb06ab6bf13f97099e1cf3d601d2bcdbaddc
Miklos Kalman <Miklos Kalman>
2016-01-27 18:28:26 +0100

    sddd

--- class-03.r
+++ class-03.r
@@ -1,3 +1,8 @@
-3+2
+library(ggplot2)
+#ggplot(mtcars, aes(x = carb)) + geom_bar()
+ggplot(mtcars, aes(x = hp)) + geom_histogram(binwidth = 50)
 
-55+5
+
+%ggplot(mtcars, aes(x = carb, y = hp)) + geom_boxplot()
+
+ggplot(mtcars, aes(x = hp, y=wt, fill=carb)) + geom_area()
